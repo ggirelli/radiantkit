ea1907a3234cd58dd0cc06408fcbcc80f29a5f99
Gabriele Girelli <gigi.ga90@gmail.com>
2020-01-16 17:03:19 +0100

    Moved regions of interest to selection module

--- radiantkit/image.py
+++ radiantkit/image.py
@@ -92,35 +92,6 @@ class MaskedImage(ImageSettings):
         super(MaskedImage, self).__init__()
         self.arg = arg
 
-class RegionOfInterest(object):
-    """docstring for RegionOfInterest"""
-    def __init__(self):
-        super(RegionOfInterest, self).__init__()
-
-class SquareOfInterest(RegionOfInterest):
-    """docstring for SquareOfInterest"""
-
-    __P1: Tuple[int] = None
-    __P2: Tuple[int] = None
-
-    def __init__(self, P1:(int,int), P2:(int,int), *args, **kwargs):
-        super(SquareOfInterest, self).__init__(*args, **kwargs)
-        assert all([c>=0 for c in P1]) and all([c>=0 for c in P2])
-        self.__P1=P1
-        self.__P2=P2
-
-class BoxOfInterest(RegionOfInterest):
-    """docstring for BoxOfInterest"""
-
-    __P1: Tuple[int] = None
-    __P2: Tuple[int] = None
-
-    def __init__(self, P1:(int,int,int), P2:(int,int,int), *args, **kwargs):
-        super(BoxOfInterest, self).__init__(*args, **kwargs)
-        assert all([c>=0 for c in P1]) and all([c>=0 for c in P2])
-        self.__P1=P1
-        self.__P2=P2
-
 def get_huygens_rescaling_factor(path: str) -> float:
     basename,ext = tuple(os.path.splitext(os.path.basename(path)))
     path = os.path.join(os.path.dirname(path), f"{basename}_history.txt")
