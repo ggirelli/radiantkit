d69de285441057242acf2104a8f8a05e197d7d6d
Gabriele Girelli <gigi.ga90@gmail.com>
2020-01-20 16:18:03 +0100

    Implemented CZI conversion

--- radiantkit/conversion.py
+++ radiantkit/conversion.py
@@ -3,14 +3,18 @@
 @contact: gigi.ga90@gmail.com
 '''
 
+import argparse
+from czifile import CziFile
 from nd2reader import ND2Reader
 from nd2reader.parser import Parser as ND2Parser
 import numpy as np
-from typing import Set
+from typing import Iterable, Set, Tuple
+import warnings
+import xml.etree.ElementTree as ET
 
 class ND2Reader2(ND2Reader):
     def __init__(self, filename):
-        super().__init__(filename)
+        super(ND2Reader2, self).__init__(filename)
 
     def field_count(self) -> int:
         if not 'v' in self.axes: return 1
@@ -47,3 +51,79 @@ class ND2Reader2(ND2Reader):
             Zdata = Zdata[Zlevels]
             return set(np.round(np.diff(Zdata), 3))
 
+class CziFile2(CziFile):
+    __pixels: np.ndarray = None
+
+    def __init__(self, filename):
+        super(CziFile2, self).__init__(filename)
+    
+    @property
+    def pixels(self) -> np.ndarray:
+        if self.__pixels is None:
+            with warnings.catch_warnings(record = True) as w:
+                self.__pixels = self.asarray()
+        return self.__pixels
+
+    def field_count(self) -> int:
+        if not "S" in self.axes: return 1
+        return self.pixels.shape[self.axes.index("S")]
+
+    def isLive(self) -> bool:
+        if "T" in self.axes: return 1 < self.shape[self.axes.index("T")]
+        return False
+
+    def is3D(self) -> bool:
+        return 'Z' in self.axes
+
+    def hasMultiChannels(self) -> bool:
+        if "C" in self.axes: return 1 < self.channel_count()
+        return False
+
+    def get_channel_names(self) -> Iterable[str]:
+        channel_path = "Metadata/DisplaySetting/Channels/Channel/DyeName"
+        for x in ET.fromstring(self.metadata()).findall(channel_path):
+            yield x.text.replace(" ", "").lower()
+
+    def channel_count(self) -> int:
+        if not 'C' in self.axes: n = 1
+        n = self.pixels.shape[self.axes.index("C")]
+        assert (len(list(self.get_channel_names())) == n
+            ), "channel count mismatch."
+        return n
+
+    def get_resolution(self) -> dict:
+        resolution_path = "Metadata/Scaling/Items/Distance"
+        resolution = []
+        for x in ET.fromstring(self.metadata()).findall(resolution_path):
+            if x.attrib['Id'] in ["X", "Y", "Z"]:
+                resolution.append((x.attrib['Id'], float(x[0].text)))
+        return dict(resolution)
+
+    def squeeze_axes(self, skip: str) -> None:
+        axes = list(self.axes)
+        for axis in axes:
+            axis_id = axes.index(axis)
+            if axis in skip: continue
+            self.__pixels = np.squeeze(self.pixels, axis_id)
+            self.shape = self.pixels.shape
+            axes.pop(axis_id)
+        self.axes = "".join(axes)
+
+    def reorder_axes(self, bundle_axes: str) -> None:
+        if self.axes == bundle_axes: return
+        bundle_axes = list(bundle_axes)
+        assert len(bundle_axes) == len(self.axes)
+        assert all([axis in self.axes for axis in bundle_axes])
+        self.__pixels = np.moveaxis(self.pixels, range(len(axes)),
+            [bundle_axes.index(axis) for axis in self.axes])
+        self.shape = self.pixels.shape
+        self.axes =  bundle_axes
+
+    def get_channel_pixels(self, args: argparse.Namespace,
+        field_id: int = None) -> Iterable[Tuple[np.ndarray, int]]:
+        if field_id is not None: field = self.pixels[field_id, :]
+        else:
+            assert "C" == self.axes[0]
+            field = self.pixels
+        for channel_id  in range(self.channel_count()):
+            yield (field[channel_id], channel_id)
--- radiantkit/scripts/czi_to_tiff.py
+++ radiantkit/scripts/czi_to_tiff.py
@@ -5,7 +5,12 @@
 
 import argparse
 import logging
+import os
+from radiantkit.conversion import CziFile2
+import radiantkit.image as imt
+from string import Template
 import sys
+from tqdm import tqdm
 
 logging.basicConfig(level=logging.INFO,
     format='%(asctime)s - %(message)s', datefmt='%m/%d/%Y %I:%M:%S')
@@ -22,9 +27,10 @@ ${channel_id}   : the channel ID (number). Leading 0s added up to 3 digits.
 ${series_id}    : the series ID (number). Leading 0s added up to 3 digits.
 ${dimensions}   : the number of dimensions in the image, followed by a "D".
 ${axes_order}   : the order of the axes in the image (e.g., "TZYX").
-
 The default template is "${channel_name}_${series_id}". Hence, when writing the
-3rd series of the "a488" channel, the output file name would be: "a488_003.tiff"
+3rd series of the "a488" channel, the output file name would be:"a488_003.tiff".
+Please, remember to escape the "$" when running from command line if using
+double quotes, i.e., "\\$". Alternatively, use single quotes, i.e., '$'.
     ''', formatter_class = argparse.RawDescriptionHelpFormatter)
 
     parser.add_argument('input', type = str,
@@ -36,7 +42,8 @@ The default template is "${channel_name}_${series_id}". Hence, when writing the
 
     parser.add_argument('-T', '--template', metavar = "template", type = str,
         help = """Template for output file name. See main description for more
-        details. Default: '${channel_name}_${series_id}'""", default = None)
+        details. Default: '${channel_name}_${series_id}'""",
+        default = "${channel_name}_${series_id}")
 
     parser.add_argument('--compressed',
         action = 'store_const', dest = 'doCompress',
@@ -52,7 +59,7 @@ The default template is "${channel_name}_${series_id}". Hence, when writing the
     if args.outdir is None:
         args.outdir = os.path.splitext(os.path.basename(args.input))[0]
         args.outdir = os.path.join(os.path.dirname(args.input), args.outdir)
-        print(f"Output directory: '{args.outdir}'")
+        logging.info(f"Output directory: '{args.outdir}'")
 
     assert os.path.isfile(args.input), f"input file not found: {args.input}"
     assert not os.path.isfile(args.outdir
@@ -61,8 +68,45 @@ The default template is "${channel_name}_${series_id}". Hence, when writing the
 
     return args
 
+def get_output_path(args: argparse.Namespace, CZI: CziFile2,
+    channel_id: int, field_id: int = 1) -> str:
+    d = {
+        'channel_name' : list(CZI.get_channel_names())[channel_id],
+        'channel_id' : f"{(channel_id+1):03d}",
+        'series_id' : f"{(field_id+1):03d}",
+        'dimensions' : 3,
+        'axes_order' : "ZYX"
+    }
+    t = Template(args.template)
+    return f"{t.safe_substitute(d)}.tiff"
+
 def run(args: argparse.Namespace) -> None:
-    pass
+    CZI = CziFile2(args.input)
+    assert not CZI.isLive(), "time-course conversion images not implemented."
+    logging.info(f"Found {CZI.field_count()} field(s) of view, " +
+        f"with {CZI.channel_count()} channel(s).")
+
+    resolution = CZI.get_resolution()
+
+    CZI.squeeze_axes("SCZYX")
+
+    if 1 == CZI.field_count(): CZI.reorder_axes("CZYX")
+    else: CZI.reorder_axes("SCZYX")
+
+    def field_generator(CZI):
+        for field_id in range(CZI.field_count()):
+            for yieldedValue in CZI.get_channel_pixels(args, field_id):
+                channel_pixels,channel_id = yieldedValue
+                yield (channel_pixels,
+                    get_output_path(args, CZI, channel_id, field_id))
+
+    for (OI, opath) in tqdm(field_generator(CZI),
+        total=CZI.field_count()*CZI.channel_count()):
+        imt.Image.save_tiff(os.path.join(args.outdir, opath),
+            OI, imt.get_dtype(OI.max()), args.doCompress, bundled_axes = "ZYX",
+            resolution = (1e-6/resolution["X"], 1e-6/resolution["Y"]),
+            inMicrons = True, ResolutionZ = resolution["Z"]*1e6,
+            forImageJ = True)
 
 def main():
-    args = parse_arguments()
+    run(parse_arguments())
