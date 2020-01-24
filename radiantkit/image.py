Fixed axes and particle size calculation

radiantkit/image.py
@@ -20,7 +20,7 @@ from typing import List, Optional, Tuple, Union
 import warnings
 
 class ImageSettings(object):
-    __ALLOWED_AXES: str = "VCTZYX"
+    _ALLOWED_AXES: str = "VCTZYX"
     _axes_order: str = "VCTZYX"
 
     def __init__(self):
@@ -37,13 +37,16 @@ class ImageBase(ImageSettings):
     def __init__(self, pixels: np.ndarray, path: Optional[str]=None,
         axes: Optional[str]=None):
         super(ImageSettings, self).__init__()
+        assert len(pixels.shape) <= len(self._ALLOWED_AXES)
         self._pixels = pixels.copy()
         self._remove_empty_axes()
         if axes is not None:
             assert len(axes) == len(self.shape)
-            assert all([c in self.__ALLOWED_AXES for c in new_axes])
+            assert all([c in self._ALLOWED_AXES for c in new_axes])
             assert all([1 == c.count(new_axes) for c in set(new_axes)])
             self._axes_order = axes
+        else:
+            self._axes_order = self._ALLOWED_AXES[-len(self.shape):]
 
     @property
     def shape(self) -> Tuple[int]:

radiantkit/selection.py
@@ -4,7 +4,7 @@
 '''
 
 import numpy as np
-from radiantkit.image import Image
+from radiantkit.image import Image, ImageBinary
 from typing import Optional, Tuple, Type
 
 class BoundingElement(object):
@@ -21,6 +21,16 @@ class BoundingElement(object):
     @property
     def shape(self) -> Tuple[int]:
         return tuple([int(b1-b0) for (b0, b1) in self._bounds])
+
+    @staticmethod
+    def from_binary_image(B: ImageBinary) -> 'BoundingElement':
+        assert 0 == B.pixels.min() and 1 == B.pixels.max()
+        axes_bounds = []
+        for axis_id in range(len(B.shape)):
+            axis = B.pixels.sum(tuple([axis for axis in range(len(B.shape))
+                if axis != axis_id])) != 0
+            axes_bounds.append((axis.argmax(), len(axis)-axis[::-1].argmax()))
+        return BoundingElement(axes_bounds)
 
     def apply(self, I: Type[Image]) -> np.ndarray:
         assert len(self._bounds) == len(I.shape)