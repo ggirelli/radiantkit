493895057d7a97a7764cb32b82ac802354def72b
Gabriele Girelli <gigi.ga90@gmail.com>
2020-01-24 15:38:57 +0100

    Setting up Particle detection

--- radiantkit/particle.py
+++ radiantkit/particle.py
@@ -3,6 +3,10 @@
 @contact: gigi.ga90@gmail.com
 '''
 
+from radiantkit.image import ImageBinary, ImageLabeled
+from radiantkit.selection import BoundingElement
+from typing import List, Type, Union
+
 class ParticleSettings(object):
 	def __init__(self):
 		super(ParticleSettings, self).__init__()
@@ -11,6 +15,30 @@ class Particle(ParticleSettings):
 	def __init__(self):
 		super(Particle, self).__init__()
 
+	@staticmethod
+	def get_bounding_element(B: ImageBinary) -> BoundingElement:
+		assert 0 == B.min() and 1 == B.max()
+		axes_bounds = []
+		for axis_id in range(len(B.shape)):
+			axis = B.sum([axis for axis in range(len(B.shape))
+				if axis != axis_id])
+			axes_bounds.append((axis.index(1), axis[::-1].index(1)))
+		return BoundingElement(axes_bounds)
+
 class Nucleus(Particle):
 	def __init__(self):
 		super(Nucleus, self).__init__()
+
+class ParticleFinder(object):
+	def __init__(self, I: Union[ImageBinary,ImageLabeled],
+		particleClass: Type[Particle] = Particle):
+		super(ParticleFinder, self).__init__()
+		if isinstance(I, ImageBinary):
+			I = ImageLabeled(I.pixels)
+
+	@staticmethod
+	def get_particles_from_labeled_image(L: ImageLabeled,
+		particleClass: Type[Particle] = Particle) -> List[Type[Particle]]:
+		assert L.min() != L.max(), 'monochromatic image detected.'
+		for lab in range(1, L.max()):
+			pass
--- radiantkit/selection.py
+++ radiantkit/selection.py
@@ -6,30 +6,10 @@
 from typing import Tuple
 
 class RegionOfInterest(object):
-    """docstring for RegionOfInterest"""
     def __init__(self):
         super(RegionOfInterest, self).__init__()
 
-class SquareOfInterest(RegionOfInterest):
-    """docstring for SquareOfInterest"""
-
-    __P1: Tuple[int] = None
-    __P2: Tuple[int] = None
-
-    def __init__(self, P1:(int,int), P2:(int,int), *args, **kwargs):
-        super(SquareOfInterest, self).__init__(*args, **kwargs)
-        assert all([c>=0 for c in P1]) and all([c>=0 for c in P2])
-        self.__P1=P1
-        self.__P2=P2
-
-class BoxOfInterest(RegionOfInterest):
-    """docstring for BoxOfInterest"""
-
-    __P1: Tuple[int] = None
-    __P2: Tuple[int] = None
-
-    def __init__(self, P1:(int,int,int), P2:(int,int,int), *args, **kwargs):
-        super(BoxOfInterest, self).__init__(*args, **kwargs)
-        assert all([c>=0 for c in P1]) and all([c>=0 for c in P2])
-        self.__P1=P1
-        self.__P2=P2
+class BoundingElement(RegionOfInterest):
+    def __init__(self, axes_bounds: Tuple[Tuple[int]]):
+        super(BoundingElement, self).__init__()
+        
