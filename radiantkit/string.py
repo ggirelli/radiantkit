af19f54311de19366f3818de31dc6e18f8a36efb
Gabriele Girelli <gigi.ga90@gmail.com>
2020-01-26 10:38:35 +0100

    Simplified by always storing start,stop tuples also for singletons

--- radiantkit/string.py
+++ radiantkit/string.py
@@ -24,10 +24,9 @@ class MultiRange(object):
         self.__extremes_list = []
         for string_range in string_range_list:
             extremes = [int(x) for x in string_range.split("-")]
-            assert len(extremes) in (1, 2), "a range should be specified as A-B"
-            extremes = list(set(extremes))
-            if 2 == len(extremes):
-                assert extremes[1] >= extremes[0]
+            if 1 == len(extremes): extremes = [extremes[0], extremes[0]]
+            assert 2 == len(extremes), "a range should be specified as A-B"
+            assert extremes[1] >= extremes[0]
             self.__extremes_list.append(tuple(extremes))
 
         self.__extremes_list = sorted(
@@ -42,54 +41,22 @@ class MultiRange(object):
             while i < len(self.__extremes_list)-1:
                 A = self.__extremes_list[i]
                 B = self.__extremes_list[i+1]
-                if 1 == len(A):
-                    if 1 == len(B):
-                        if A == B:
-                            self.__extremes_list.pop(i)
-                            popped = 1
-                            break
-                        elif A[0] == B[0]-1:
-                            self.__extremes_list[i] = (A[0], B[0])
-                            self.__extremes_list.pop(i+1)
-                            popped = 1
-                            break
-                    elif 2 == len(B):
-                        if A[0] >= B[0]-1:
-                            self.__extremes_list[i+1] = (A[0], B[1])
-                            self.__extremes_list.pop(i)
-                            popped = 1
-                            break
-                elif 2 == len(A):
-                    if 1 == len(B):
-                        if B[0] in (A[1], A[1]+1):
-                            self.__extremes_list[i] = (A[0], B[0])
-                            self.__extremes_list.pop(i+1)
-                            popped = 1
-                            break
-                        elif A[1] > B[0]:
-                            self.__extremes_list.pop(i+1)
-                            popped = 1
-                            break
-                    elif 2 == len(B):
-                        if A[1] >= B[0] and A[1] < B[1]:
-                            self.__extremes_list[i] = (A[0], B[1])
-                            self.__extremes_list.pop(i+1)
-                            popped = 1
-                            break
-                        elif A[1] >= B[1]:
-                            self.__extremes_list.pop(i+1)
-                            popped = 1
-                            break
+                if A[1] >= B[0] and A[1] < B[1]:
+                    self.__extremes_list[i] = (A[0], B[1])
+                    self.__extremes_list.pop(i+1)
+                    popped = 1
+                    break
+                elif A[1] >= B[1]:
+                    self.__extremes_list.pop(i+1)
+                    popped = 1
+                    break
                 i += 1
             if i >= len(self.__extremes_list)-2+popped: is_clean = True
 
     def __next__(self) -> int:
         for extremes in self.__extremes_list:
-            if 1 == len(extremes):
-                yield extremes[0] - self.zero_indexed
-            else:
-                for i in range(extremes[0], extremes[1]+1):
-                    yield i - self.zero_indexed
+            for i in range(extremes[0], extremes[1]+1):
+                yield i - self.zero_indexed
 
     def __iter__(self) -> Iterator[int]:
         return self.__next__()
