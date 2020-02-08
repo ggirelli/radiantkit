e7815bde7044b61ebcc4d73b6205f96c2dc63fb3
Gabriele Girelli <gigi.ga90@gmail.com>
2020-02-08 12:34:37 +0100

    Added test example.

---
+++ radiantkit/test/test_stat.py
@@ -0,0 +1,11 @@
+'''
+@author: Gabriele Girelli
+@contact: gigi.ga90@gmail.com
+'''
+
+from radiantkit import stat
+
+
+def test_quantile_from_counts():
+    assert 1.5 == stat.quantile_from_counts(
+        [1, 2, 3, 4, 5], [1, 1, 1, 1, 1], .25)
