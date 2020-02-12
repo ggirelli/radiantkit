c049d1084d76a38994c4ad1b8a11db9447399824
Gabriele Girelli <gigi.ga90@gmail.com>
2020-02-12 14:29:12 +0100

    Added missing __all__ variable

--- radiantkit/__init__.py
+++ radiantkit/__init__.py
@@ -5,5 +5,12 @@
 
 from radiantkit.const import __version__
 from radiantkit import const, scripts
-from radiantkit import conversion, image, particle, segmentation, series
+from radiantkit import conversion, segmentation
+from radiantkit import image, particle, series
 from radiantkit import path, plot, report, stat, string
+
+__all__ = ["__version__",
+           "const", "scripts",
+           "conversion", "segmentation",
+           "image", "particle", "series",
+           "path", "plot", "report", "stat", "string"]
--- radiantkit/scripts/__init__.py
+++ radiantkit/scripts/__init__.py
@@ -7,3 +7,8 @@ from radiantkit.scripts import czi_to_tiff, nd2_to_tiff
 from radiantkit.scripts import extract_objects, select_nuclei
 from radiantkit.scripts import tiff_split
 from radiantkit.scripts import tiff_findoof, tiff_segment, tiffcu
+
+__all__ = ["czi_to_tiff", "nd2_to_tiff",
+           "extract_objects", "select_nuclei",
+           "tiff_split",
+           "tiff_findoof", "tiff_segment", "tiffcu"]
