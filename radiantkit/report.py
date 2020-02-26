ec7ad7dd01d62bceaa8e1c28680aac89438dbd99
Gabriele Girelli <gigi.ga90@gmail.com>
2020-02-26 14:13:25 +0100

    Implemented snakemake pipeline setup script.

--- radiantkit/report.py
+++ radiantkit/report.py
@@ -14,24 +14,36 @@ from radiantkit import series
 from typing import Dict, Tuple
 
 
-class Report(object):
+class JinjaTemplate(object):
     _env: jj2.Environment
     _template: jj2.Template
 
     def __init__(self, template: str):
-        super(Report, self).__init__()
+        super(JinjaTemplate, self).__init__()
         self._env = jj2.Environment(
                 loader=jj2.PackageLoader('radiantkit', 'templates'),
                 autoescape=jj2.select_autoescape(['html', 'xml'])
             )
+        self._template = self._env.get_template(template)
+
+    def render(self, path: str, **kwargs) -> None:
+        with open(path, "w+") as OH:
+            OH.write(self._template.render(**kwargs))
+
+
+class Report(JinjaTemplate):
+    _env: jj2.Environment
+    _template: jj2.Template
+
+    def __init__(self, template: str):
+        super(Report, self).__init__(template)
         self._env.filters['basename'] = os.path.basename
         self._env.filters['dirname'] = os.path.dirname
         self._template = self._env.get_template(template)
 
     def render(self, path: str, **kwargs) -> None:
         assert "title" in kwargs
-        with open(path, "w+") as OH:
-            OH.write(self._template.render(**kwargs))
+        super(Report, self).render(path, **kwargs)
 
 
 def report_select_nuclei(
--- radiantkit/scripts/pipeline.py
+++ radiantkit/scripts/pipeline.py
@@ -5,7 +5,9 @@
 
 import argparse
 import logging
+import os
 from radiantkit.const import __version__
+from radiantkit.pipeline import setup_workflow
 import sys
 
 logging.basicConfig(
@@ -22,17 +24,22 @@ def init_parser(subparsers: argparse._SubParsersAction
         help="Setup vanilla snakemake workflows. *NOT IMPLEMENTED*")
 
     parser.add_argument(
-        'input', type=str,
-        help='''Path to the czi file to convert.''')
+        'workflow', metavar="name", type=str, choices=["yfish"],
+        help='''Name of the workflow. Available: yfish''')
+    parser.add_argument(
+        'root', type=str,
+        help='''Path to folder where to set up the workflow.''')
 
     parser.add_argument(
-        '-o', '--outdir', metavar="outdir", type=str, default=None,
-        help="""Path to output TIFF folder, created if missing. Default to a
-        folder with the input file basename.""")
+        '--conda-env', type=str, metavar='STRING', help='''Name of the
+        conda environment where radiantkit is installed to.''')
+    parser.add_argument(
+        '--shell-type', type=str, metavar='STRING',
+        help='''Shell type where to run snakemake. One of: bash, zsh.''')
 
     parser.add_argument(
-        '-C', '--compressed', action='store_const', dest='doCompress',
-        const=True, default=False, help='Force compressed TIFF as output.')
+        '-f', action='store_const', help="""Overwrite existing folder.""",
+        const=True, default=False)
 
     parser.add_argument('--version', action='version',
                         version=f'{sys.argv[0]} {__version__}')
@@ -43,8 +50,19 @@ def init_parser(subparsers: argparse._SubParsersAction
 
 
 def parse_arguments(args: argparse.Namespace) -> argparse.Namespace:
+    assert not os.path.isfile(args.root), "cannot create workflow folder"
+    assert not os.path.isdir(args.root) or args.f, (
+        "workflow folder already exists")
+    if not os.path.isdir(args.root):
+        os.mkdir(args.root)
     return args
 
 
 def run(args: argparse.Namespace) -> None:
-    raise NotImplementedError
+    setup_workflow(args.root, args.workflow)
+    print(f"\nPipeline set up in '{args.root}'.\nNext:\n")
+    config_path = os.path.join(args.root, f"{args.workflow}.config.yaml")
+    print(f"(1) Manually update the config file, for example with:")
+    print(f"nano '{config_path}'\n")
+    print(f"(2) Then, run the pipeline with:")
+    print(f"{os.path.join(args.root, 'run.sh')}\n")
