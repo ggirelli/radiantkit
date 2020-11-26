d610a1db18b9eef8dd185ff42ce638d88def8dc7
Gabriele Girelli <gigi.ga90@gmail.com>
2020-11-26 10:01:42 +0100

    removed templates for pipeline maker

--- radiantkit/templates/snakemake/run.sh
+++
@@ -1,5 +0,0 @@
-{% if shell_type is not none: %}{{shell_type}}{% endif %}
-{% if conda_env is not none:%}conda init
-conda activate {{conda_env}}{% endif %}
-cd {{root_folder}}
-snakemake --configfile {{name}}.config.yaml -s {{name}}.snakefile
