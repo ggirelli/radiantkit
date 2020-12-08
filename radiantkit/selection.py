7a30b4345007615620e42d7bb9832aa72450d790
Gabriele Girelli <gigi.ga90@gmail.com>
2020-12-08 18:55:08 +0100

    Refactored, now using slices.

--- radiantkit/selection.py
+++ radiantkit/selection.py
@@ -8,43 +8,40 @@ from radiantkit.image import Image, ImageBinary
 from typing import Tuple
 
 
-point = Tuple[int, int]
-
-
 class BoundingElement(object):
-    _bounds: Tuple[point, ...]
+    _bounds: Tuple[slice, ...]
 
-    def __init__(self, axes_bounds: Tuple[point, ...]):
+    def __init__(self, axes_bounds: Tuple[slice, ...]):
         super(BoundingElement, self).__init__()
         self._bounds = axes_bounds
 
     @property
-    def bounds(self) -> Tuple[point, ...]:
+    def bounds(self) -> Tuple[slice, ...]:
         return self._bounds
 
     @property
     def shape(self) -> Tuple[int, ...]:
-        return tuple([int(b1 - b0) for (b0, b1) in self.bounds])
+        return tuple([int(r.stop - r.start) for r in self.bounds])
 
     @staticmethod
     def from_binary_image(B: ImageBinary) -> "BoundingElement":
         assert 0 == B.pixels.min() and 1 == B.pixels.max()
         axes_bounds = []
         for axis_id in range(len(B.shape)):
-            axis = (
-                B.pixels.sum(
-                    tuple([axis for axis in range(len(B.shape)) if axis != axis_id])
+            axes_to_sum = list(range(len(B.shape)))
+            axes_to_sum.pop(axes_to_sum.index(axis_id))
+            axis_projection = B.pixels.sum(axes_to_sum) != 0
+            axes_bounds.append(
+                slice(
+                    axis_projection.argmax(),
+                    len(axis_projection) - axis_projection[::-1].argmax(),
                 )
-                != 0
             )
-            axes_bounds.append((axis.argmax(), len(axis) - axis[::-1].argmax()))
         return BoundingElement(tuple(axes_bounds))
 
     def apply(self, img: Image) -> np.ndarray:
         assert len(self._bounds) == len(img.shape)
-        for axis_id in range(len(img.shape)):
-            assert self._bounds[axis_id][1] <= img.shape[axis_id]
-        return img.pixels[tuple([slice(b0, b1) for b0, b1 in self._bounds])].copy()
+        return img.pixels[self._bounds].copy()
 
     def __repr__(self):
         return f"{len(self._bounds)}D Bounding Element: {self._bounds}"
