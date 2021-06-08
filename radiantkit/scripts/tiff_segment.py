fd679d38c2524c1ef40308ee7650db5b4f3e52d3
Gabriele Girelli <gigi.ga90@gmail.com>
2021-06-08 08:02:36 +0200

    Added --most-objects option to tiff_segment

--- radiantkit/scripts/tiff_segment.py
+++ radiantkit/scripts/tiff_segment.py
@@ -17,7 +17,8 @@ from radiantkit.io import add_log_file_handler
 import re
 from rich.progress import track  # type: ignore
 from rich.prompt import Confirm  # type: ignore
-from typing import Optional
+import skimage as ski  # type: ignore
+from typing import List, Optional, Tuple
 
 
 @enable_rich_exceptions
@@ -123,6 +124,8 @@ Input images that have the specified prefix and suffix are not segmented.""",
         default=False,
         help="""Remove objects touching the bottom/top of the stack.""",
     )
+
+    advanced = parser.add_argument_group("slice arguments")
     parser.add_argument(
         "--only-focus",
         action="store_const",
@@ -130,6 +133,14 @@ Input images that have the specified prefix and suffix are not segmented.""",
         const=True,
         default=False,
         help="""Export mask for the most in-focus slice only.""",
+    )
+    parser.add_argument(
+        "--most-objects",
+        action="store_const",
+        dest="most_objects",
+        const=True,
+        default=False,
+        help="""Export mask only for the slice with most objects.""",
     )
 
     advanced = parser.add_argument_group("advanced arguments")
@@ -249,6 +260,10 @@ def parse_arguments(args: argparse.Namespace) -> argparse.Namespace:
     loglvl = logging.DEBUG if args.debug_mode else 20
     logging.getLogger().level = logging.CRITICAL if args.silent else loglvl
 
+    assert not (
+        args.only_focus and args.most_objects
+    ), "cannot use --only-focus and --most-objects together."
+
     return args
 
 
@@ -274,6 +289,7 @@ def print_settings(args: argparse.Namespace, clear: bool = True) -> str:
                Clear Z : {args.do_clear_Z}
 
          Only in-focus : {args.only_focus}
+          Most objects : {args.most_objects}
           Default axes : {args.default_axes}
                Rescale : {args.do_rescaling}
                Threads : {args.threads}
@@ -316,6 +332,49 @@ def read_mask_2d(
     return mask2d
 
 
+def extract_slice(pixels: np.ndarray, z_index: int, i: int = 0) -> np.ndarray:
+    slice_condition = np.indices(pixels.shape)[z_index] == i
+    new_shape = list(pixels.shape[:z_index])
+    new_shape.extend(pixels.shape[(z_index + 1) :])
+    return np.extract(slice_condition, pixels).reshape(new_shape)
+
+
+def select_in_focus_slice(
+    img: channel.ImageGrayScale, L: channel.ImageLabeled
+) -> channel.ImageLabeled:
+    z_index = img.axes.index("Z")
+    if z_index >= 0:
+        L = channel.ImageLabeled(extract_slice(L, z_index, img.focus_slice_id()))
+    return L
+
+
+def select_most_populated_slice(
+    img: channel.ImageGrayScale, L: channel.ImageLabeled
+) -> channel.ImageLabeled:
+    z_index = img.axes.index("Z")
+    if z_index >= 0:
+        object_counts: List[int] = []
+        for slice_id in range(img.shape[z_index]):
+            object_counts.append(
+                ski.measure.label(extract_slice(L, z_index, slice_id)).max()
+            )
+        max_objects: int = max(object_counts)
+        if 1 == object_counts.count(max_objects):
+            return extract_slice(L, z_index, object_counts.index(max_objects))
+        else:
+            distances_from_focus: List[Tuple[int, int]] = [
+                (i, abs(i - img.focus_slice_id()))
+                for i in range(len(object_counts))
+                if object_counts[i] == max_objects
+            ]
+            most_populated_closest_to_focus: int = sorted(
+                distances_from_focus,
+                key=lambda x: x[1],
+            )[0][0]
+            return extract_slice(L, z_index, most_populated_closest_to_focus)
+    return L
+
+
 def run_binarizer(
     args: argparse.Namespace, imgpath: str, img: channel.ImageGrayScale
 ) -> channel.ImageLabeled:
@@ -344,14 +403,10 @@ def run_binarizer(
 
     if args.only_focus:
         logging.info("selecting in-focus slice.")
-        z_index = img.axes.index("Z")
-        if z_index >= 0:
-            slice_condition = np.indices(img.shape)[z_index] == img.focus_slice_id()
-            new_shape = list(img.shape[:z_index])
-            new_shape.extend(img.shape[(z_index + 1) :])
-            L = channel.ImageLabeled(
-                np.extract(slice_condition, L.pixels).reshape(new_shape)
-            )
+        L = select_in_focus_slice(img, L)
+    elif args.most_objects:
+        logging.info("selecting most populated slice.")
+        L = select_most_populated_slice(img, L)
 
     if M2D is not None:
         logging.info("recovering labels from 2D mask")
