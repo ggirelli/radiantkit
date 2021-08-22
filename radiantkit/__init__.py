ae46871d67502b98ac0f2a36d158464150f71479
Gabriele Girelli <gigi.ga90@gmail.com>
2021-08-22 22:29:25 +0200

    Added support for autocompletion in bash, fish, and zsh

--- radiantkit/__init__.py
+++ radiantkit/__init__.py
@@ -7,6 +7,7 @@ from radiantkit import const
 from radiantkit import conversion
 
 from importlib.metadata import version
+from typing import List
 
 try:
     __version__ = version(__name__)
@@ -14,3 +15,4 @@ except Exception as e:
     raise e
 
 __all__ = ["__version__", "const", "conversion"]
+__path__: List[str]
