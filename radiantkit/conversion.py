c332c50dd5d9a6e62e6e980422b83218a23a995a
Gabriele Girelli <gigi.ga90@gmail.com>
2021-11-08 20:09:30 +0100

    feat: added field shakiness

--- radiantkit/conversion.py
+++ radiantkit/conversion.py
@@ -67,8 +67,12 @@ class ND2Reader2(ND2Reader):
         for field_id in range(self.field_count()):
             z_steps_hist = stat.list_to_hist(self.get_field_resolutionZ(field_id))
             z_mode = stat.get_hist_mode(z_steps_hist)
-            logger.info(f"\tF#{field_id} - Delta Z (value um, count): {z_steps_hist}")
-            logger.info(f"\tF#{field_id} - Delta Z (mode): {z_mode} um")
+            logger.info(f"Delta Z\tF#{field_id}: {z_mode} um; {z_steps_hist}")
+            shakiness = (
+                sum([v for k, v in z_steps_hist.items() if k != z_mode])
+                / self.sizes["z"]
+            )
+            logger.info(f"Shakiness\tF#{field_id}: {shakiness}%")
 
     @property
     def xy_resolution(self) -> float:
